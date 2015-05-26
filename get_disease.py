ec8d3e82b51b9fda85b92e87ddcf39430a5e5c46
Kang Liu <kgliu0101@gmail.com>
2015-05-26 15:34:46 -0700

    convert tab to 4 spaces for indent
    in all python scripts

--- get_disease.py
+++ get_disease.py
@@ -21,39 +21,38 @@ omimMulti = 0
 synonymNum = 0
 synonymMulti = 0
 for dis in disorders:
-	#synonym = ''
-	synonym = []
-	omim = []
-	omimFound = False
-	synFound = False
+    synonym = []
+    omim = []
+    omimFound = False
+    synFound = False
 
-	for child in dis.getchildren():
-		syns = child.findall('.//Synonym')
+    for child in dis.getchildren():
+        syns = child.findall('.//Synonym')
 
-		for syn in syns:
-			synonym.append(syn.text)
+        for syn in syns:
+            synonym.append(syn.text)
 
-		refs = child.findall('.//ExternalReference')
-		for ref in refs:
-			if ref.find('Source').text == 'OMIM':
-				omim.append(ref.find('Reference').text)
+        refs = child.findall('.//ExternalReference')
+        for ref in refs:
+            if ref.find('Source').text == 'OMIM':
+                omim.append(ref.find('Reference').text)
 
-	newdata = {
-		"ORDOID": dis.find('OrphaNumber').text,
-		"FullName": dis.find('Name').text,
-#		"NameLowercase": dis.find('Name').text.lower(),
-		"Synonym": synonym,
-#		"SynonymLowercase": synonym.lower(),
-		"Type": dis.find('DisorderType/Name').text,
-		"OMIMID": omim,
-		"Active": "Yes"
-	}
-	collection.save(newdata)
+    newdata = {
+        "ORDOID": dis.find('OrphaNumber').text,
+        "FullName": dis.find('Name').text,
+#       "NameLowercase": dis.find('Name').text.lower(),
+        "Synonym": synonym,
+#       "SynonymLowercase": synonym.lower(),
+        "Type": dis.find('DisorderType/Name').text,
+        "OMIMID": omim,
+        "Active": "Yes"
+    }
+    collection.save(newdata)
 
-	if len(omim) >0: omimNum += 1
-	if len(omim) > 1: omimMulti += 1
-	if len(synonym) > 0: synonymNum +=1
-	if len(synonym) > 1: synonymMulti += 1
+    if len(omim) >0: omimNum += 1
+    if len(omim) > 1: omimMulti += 1
+    if len(synonym) > 0: synonymNum +=1
+    if len(synonym) > 1: synonymMulti += 1
 
 print len(disorders)
 print omimNum
